982003623e7a8867859e67cb75fe223776b0b9bc
malon153 <malon153@deathstar.morris.umn.edu>
2013-04-04 20:22:53 -0500

    copied MuLambda searchers, added crossover functionality to them. Changed SymbolicRegression.quality to use absolute values errors instead of squaring the error.

--- EssentialsOfMetaheuristics/src/problems/SymbolicRegression.groovy
+++ EssentialsOfMetaheuristics/src/problems/SymbolicRegression.groovy
@@ -7,9 +7,9 @@ import utility.RandomGenerator;
 class SymbolicRegression extends TreeProblem {
 
 	def evalCount = 0
-	def maxIterations = 1000
+	def maxIterations = 4000
 
-	def problemFunction = {vars-> 100*Math.sin(vars.getAt('x')^2)}
+	def problemFunction = {vars-> 10*Math.sin((vars.getAt('x')/5)**2)}
 
 	def functions = [
 		def add = new FunctionArityPair(function : {array -> array[0] + array[1]}, arity : 2, string : "+"),
@@ -20,27 +20,27 @@ class SymbolicRegression extends TreeProblem {
 				return 1
 			}else{array[0]/array[1]}}, arity : 2, string : "/"),
 		def cosine = new FunctionArityPair(function : {array -> Math.cos(array[0])}, arity : 1, string : "cos"),
-		//def sine = new FunctionArityPair(function : {array -> Math.sin(array[0])}, arity : 1, string : "sin"),
+		def sine = new FunctionArityPair(function : {array -> Math.sin(array[0])}, arity : 1, string : "sin"),
 		def log = new FunctionArityPair(function : {array ->
 			if(array[0] <= 0){
 				return 1
 			}else{Math.log(array[0])}}, arity :1, string: "log")
 	]
 
-	def terminals = [ {-> "x"}, {-> 5}, {-> 10}, {-> 32}, {-> rand.nextInt(4)}]
+	def terminals = [ {-> "x"}, {-> 5}, {-> 10}, {-> 32}, {-> rand.nextInt(15)}]
 
-	def testPoints =[['x' : 2], ['x':3], ['x':4],['x':5],['x':6],['x':7],['x':8],['x':9],['x':10]]
+	def testPoints =[['x' : 1], ['x' : 2], ['x':3], ['x':4],['x':5],['x':6],['x':7],['x':8],['x':9],['x':10]]
 
 	def quality = {a ->
 		evalCount++
 		def qual = 0
 		testPoints.each{vars ->
-			qual += (a.evaluate(vars) - problemFunction(vars))**2
+			qual += Math.abs(a.evaluate(vars) - problemFunction(vars))
 		}
 		0 - qual //invert, to make problem a maximization
 	}
 
 	def String toString(){
-		"SymbReg:Sin(x^2)"
+		"SymbReg:10*Sin((x/5)^2)"
 	}
 }
